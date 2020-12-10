6d92a01d8a74fc4b25654e0603481343bfacc995
Aidar Rakhamtullin <mr.darik88@gmail.com>
2020-12-11 01:59:40 +0300

    initial commit

--- TrafficLightSwiftUI/ColoredCircle.swift
+++ TrafficLightSwiftUI/ColoredCircle.swift
@@ -8,18 +8,17 @@
 import SwiftUI
 
 struct ColoredCircle: View {
+    
     let color: UIColor
     var opacity: Double = 0.3
     
-//    let alpha: CGFloat
     var body: some View {
         Color(color)
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .overlay(Circle().stroke(Color.white,lineWidth: 4))
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             .frame(width: 100, height: 100)
             .opacity(opacity)
-        
     }
 }
 
